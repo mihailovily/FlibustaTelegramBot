2edbb578b01c8225fac5f8d86ea11beea474ab40
Ilya <37973985+mihailovily@users.noreply.github.com>
2023-04-05 06:49:59 +0300

    Исправил опечатки

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# Ask the user for their name
 
 echo Впиши токен telegram бота
 
@@ -23,5 +22,5 @@ pip install -r requirements.txt
 
 echo Зависимости установлены
 
-echo Для запуска воспользуйтесь командойэ
+echo Для запуска воспользуйтесь командой
 echo python3 main.py
